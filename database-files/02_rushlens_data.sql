10e059c61d1ec4760ae776748f0f8124b250ffed
Le Pham <pham.le1@northeastern.edu>
2025-12-04 08:54:23 -0500

    rushlens generated data

---
+++ database-files/02_rushlens_data.sql
@@ -0,0 +1,34 @@
+INSERT INTO Location (location_id, name, address, building, floor, location_type) 
+VALUES
+(1, 'Popeyes', '360 Huntington Ave, Boston, MA 02115', 'Curry Student Center', 1, 'Fast-Food');
+(2, 'Starbucks', '360 Huntington Ave, Boston, MA 02115', 'Curry Student Center', 1, 'Cafe');
+(3, 'D''Angelo', '360 Huntington Ave, Boston, MA 02115', 'Curry Student Center', 1, 'Fast-Food');
+(4, 'Sal''Pizza', '360 Huntington Ave, Boston, MA 02115', 'Curry Student Center', 1, 'Fast-Food');
+(5, 'Halal Shack', '360 Huntington Ave, Boston, MA 02115', 'Curry Student Center', 1, 'Fast-Food');
+(6, 'Dunkin''', '360 Huntington Ave, Boston, MA 02115', 'Hayden Hall', 1, 'Cafe');
+(7, 'Dunkin''', '115 Forsyth St, Boston, MA 02115', 'Shillman Hall', 1, 'Cafe');
+(8, 'Saxbys Northeastern', '815 Columbus Ave, Boston, MA 02115', 'EXP', 2, 'Cafe');
+(9, 'Subway', 'Leon St, Boston, MA 02115', 'Ryder Hall', 1, 'Fast-Food');
+(10, 'Kigo Kitchen', '360 Huntington Ave, Boston, MA 02115', 'Curry Student Center', 1, 'Fast-Food');
+(11, 'Equator Coffees', '360 Huntington Ave, Boston, MA 02115', 'Snell Library', 1, 'Cafe');
+(12, 'Juicygreens', '1155 Tremont St, Boston, MA 02115', 'International Village', 1, 'Drinks');
+(13, 'Fuel America', '815 Columbus Ave, Boston, MA 02115', 'ISEC', 1, 'Cafe');
+(14, '', '95456 Mallory Street', 'PO Box 43900', 'PO Box 41674', '');
+(15, '', '5868 Russell Plaza', '14th Floor', 'Suite 48', '');
+(16, '', '1758 Mitchell Place', 'Suite 92', 'Suite 6', '');
+(17, '', '93 Walton Avenue', 'Suite 94', 'PO Box 84021', '');
+(18, '', '7390 Fairview Street', '9th Floor', 'Room 179', '');
+(19, '', '086 John Wall Park', '17th Floor', '10th Floor', '');
+(20, '', '42608 Eggendart Road', 'Room 994', 'Apt 1028', '');
+(21, '', '75 Beilfuss Crossing', 'PO Box 35334', 'Apt 176', '');
+(22, '', '5586 Mcbride Pass', 'Room 1284', '20th Floor', '');
+(23, '', '734 Utah Center', 'Apt 204', 'Suite 60', '');
+(24, '', '653 Alpine Lane', 'Apt 1391', '2nd Floor', '');
+(25, '', '802 Merrick Hill', 'Room 643', 'Apt 1476', '');
+(26, '', '37045 Cody Court', 'Apt 1363', 'Room 825', '');
+(27, '', '1 Ridgeway Alley', 'PO Box 57451', '2nd Floor', '');
+(28, '', '626 Steensland Place', 'Suite 29', 'Room 1558', '');
+(29, '', '6 Welch Pass', '13th Floor', '15th Floor', '');
+(30, '', '060 Fallview Avenue', '5th Floor', 'Suite 45', '');
+
+
