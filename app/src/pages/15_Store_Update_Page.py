5da221cc1fa79a0fe7ee6687af7a3240135b4184
shreyaselva <shreya.selvaraju2024@gmail.com>
2025-12-08 16:54:38 -0500

    commit

--- app/src/pages/15_Store_Update_Page.py
+++ app/src/pages/15_Store_Update_Page.py
@@ -49,4 +49,4 @@ with st.form("update_store_form"):
                 st.error("Error connecting to API.")
 
 if st.button("Return to Store Homepage"):
-    st.switch_page("rushlens_pages/13_Store_Owner_Home.py")
+    st.switch_page("app/src/pages/15_Store_Update_Page.py")
