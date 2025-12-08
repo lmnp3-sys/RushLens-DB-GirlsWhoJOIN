0c8f2b7e0dd8929b40c5bbcc5530002d97e98a02
shreyaselva <shreya.selvaraju2024@gmail.com>
2025-12-08 18:14:02 -0500

    sample data maybe

--- app/src/pages/16_Store_Community_Data.py
+++ app/src/pages/16_Store_Community_Data.py
@@ -7,6 +7,13 @@ st.title('Community Foot Traffic Trends')
 
 API_BASE = "http://localhost:4000"
 
+data = [
+    {"hour": 8, "avg_visitors": 12, "total_customers": 12, "avg_wait_min": 5, "store_name": "Store A"},
+    {"hour": 9, "avg_visitors": 20, "total_customers": 20, "avg_wait_min": 7, "store_name": "Store B"},
+    {"hour": 10, "avg_visitors": 18, "total_customers": 18, "avg_wait_min": 6, "store_name": "Store C"},
+    {"hour": 11, "avg_visitors": 25, "total_customers": 25, "avg_wait_min": 10, "store_name": "Store D"},
+]
+
 try:
     response = requests.get(API_BASE)
     data = response.json()
