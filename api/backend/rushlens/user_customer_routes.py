3fc2dfcc448058531fe965f1ccf5657422ab527c
vaiunnithan <unnithan.v@northeastern.edu>
2025-12-08 21:22:39 -0500

    Fixing routes

--- api/backend/rushlens/user_customer_routes.py
+++ api/backend/rushlens/user_customer_routes.py
@@ -1,7 +1,7 @@
 # BLUEPRINT 3 User/Customer
 
 from flask import Blueprint, request, jsonify
-from ..db_connection import db
+from db_connection import db
 from mysql.connector import Error
 
 user_customer = Blueprint("user_customer", __name__)
